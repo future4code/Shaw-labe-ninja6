7be30240eb75c007f4990b79ad0db9b5fc7cea01
Matheus Souza <matheus92as@gmail.com>
2022-04-13 11:03:46 -0300

    Ajuste em Header e Footer

--- src/App.js
+++ src/App.js
@@ -2,37 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 import ListaServicos from './components/ListaServicos/ListaServicos'
 import { header,url } from './constants/authorization'
-
 import Cadastro from './pages/Cadastro/Cadastro'
-
 import Inicial from './pages/Inicial/Inicial'
-
-// import Headers from './components/Headers/Headers'
-
-import {Headers} from './components/Headers/Headers'
+import Headers from './components/Headers/Headers'
 import {Footers} from './components/Footer/Footers'
-
 
 const MainContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
-	/* height: 100vh; */
 	width: 100vw;
 `
-// const Header = styled.div`
-// 	width: 100%;
-// 	height: 100px;
-// 	background-color: lightgray;
-// `
+const Header = styled.div`
+	width: 100%;
+	height: 100px;
+	background-color: lightgray;
+`
 const Main = styled.div`
 	width: 100%;
 `
-// const Footer = styled.div`
-// 	width: 100%;
-// 	height: 100px;
-// 	background-color: lightgray;
-// `
+const Footer = styled.div`
+	width: 100%;
+	height: 100px;
+	background-color: lightgray;
+`
 
 class App extends React.Component {
 
@@ -41,29 +34,16 @@ class App extends React.Component {
 		console.log(url);
 		return (
 			<MainContainer>
-
 				<Header>
-				{/* <Headers/> */}
+					<Headers/>
 				</Header>
-
-				<Headers>
-
-				</Headers>
-
 				<Main>
-
-				
-				<Cadastro />
-
-
-					<Inicial>
-
-					</Inicial>
-
+					<Cadastro/>
+					<Inicial/>	
 				</Main>
-				<Footers>
-
-				</Footers>
+				<Footer>
+					<Footers/>
+				</Footer>
 			</MainContainer>
 		)
 	}
--- src/components/Headers/Headers.js
+++ src/components/Headers/Headers.js
@@ -1,55 +1,49 @@
 import React from "react";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import styled from 'styled-components'
-
-
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import styled from "styled-components";
 
 const Header = styled.div`
-	width: 100%;
-	height: 100px;
-	background-color: lightgray;
+  width: 100%;
+  height: 100px;
+  background-color: lightgray;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+// const Icone = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   align-items: center;
+//   margin: 50px;
+// `;
+const Titulo = styled.div`
+  margin: 40px;
+`;
+const Ninja = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-`
-const Icone = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
+    width: 30%;
+    margin-right: 30px;
+`;
 
-
-
-margin: 50px;
-
-`
-
-const Titulo = styled.div`
-
-margin: 40px;
-
-`
-
-const Ninja = styled.div`
-
-padding-left: 60%;
-`
-
-
-class Headers extends React.Component{
-    
-    render(){
-        return<Header>
-            <Titulo>
-            <h1>LabeNinjas</h1>
-            </Titulo>
-           <Ninja><h2>Seja um Ninja</h2></Ninja>
-            <Icone>
-            <ShoppingCartIcon fontSize="large"/>
-            </Icone>
-            </Header>
-
-    }
+class Headers extends React.Component {
+  render() {
+    return (
+      <Header>
+        <Titulo>
+          <h1>LabeNinjas</h1>
+        </Titulo>
+        <Ninja>
+          <h3>Seja um Ninja</h3>
+          <ShoppingCartIcon fontSize="large" />
+        </Ninja>
+        {/* <Icone>
+          
+        </Icone> */}
+      </Header>
+    );
+  }
 }
 
-
-export default Headers
+export default Headers;
