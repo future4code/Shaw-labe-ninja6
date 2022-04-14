f8a3e59ce60b4acd5641298ae4f7d1c2c0f11e1d
Sabrina Pires <sabrinapires_@outlook.com>
2022-04-14 17:43:38 -0300

    Carrinho funcional

--- src/components/ListaServicos/ListaServicos.js
+++ src/components/ListaServicos/ListaServicos.js
@@ -1,86 +1,88 @@
-import Axios from 'axios';
-import React from 'react'
-import Filtro from '../FiltrosDeBusca/FiltrosDeBusca'
-import { url, header } from '../../constants/authorization'
-import carrinho from '../../assets/img/carrinho.png'
-import { MainContainer, CardBox, ListaServico } from './styled'
-import Ordenação from '../Ordenação/Ordenação';
+import axios from "axios";
+import React from "react";
+import Filtro from "../FiltrosDeBusca/FiltrosDeBusca";
+import { url, header } from "../../constants/authorization";
+import carrinho from "../../assets/img/carrinho.png";
+import { MainContainer, CardBox, ListaServico } from "./styled";
+import Ordenação from "../Ordenação/Ordenação";
 
 class ListaServicos extends React.Component {
+  conversorData = (data) => {
+    //a data originalmente vem muito extensa
+    const dia = data.substring(8, 10); //então pegamos esse valor e
+    const mes = data.substring(5, 7); //cortamos oque vem antes e depois
+    const ano = data.substring(0, 4); //dos valores que precisamos
+    return `${dia}/${mes}/${ano}`; //e retornamos eles com as barras de datas
+  };
 
-    conversorData = (data) => { //a data originalmente vem muito extensa
-        const dia = data.substring(8, 10) //então pegamos esse valor e 
-        const mes = data.substring(5, 7) //cortamos oque vem antes e depois 
-        const ano = data.substring(0, 4) //dos valores que precisamos
-        return `${dia}/${mes}/${ano}`//e retornamos eles com as barras de datas
-    }
+  render() {
+    const listaDeServicos = this.props.servicos
+      .filter((servico) => {
+        return this.props.min === "" || servico.price >= this.props.min;
+      })
+      .filter((servico) => {
+        return this.props.max === "" || servico.price <= this.props.max;
+      })
+      .filter((servico) => {
+        return servico.title
+          .toLowerCase()
+          .includes(this.props.nome.toLowerCase());
+      })
+      .sort((a, b) => {
+        switch (this.props.valorDeSorteio) {
+          case "titulo":
+            return this.props.ordem * a.title.localeCompare(b.title);
+          case "data":
+            return (
+              this.props.ordem *
+              (new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime())
+            );
+          default:
+            return this.props.ordem * (a.price - b.price);
+        }
+      })
 
-    render() {
-
-        const listaDeServicos = this.props.servicos.filter((servico) => {
-            return this.props.min === "" || servico.price >= this.props.min;
-        })
-            .filter((servico) => {
-                return this.props.max === "" || servico.price <= this.props.max;
-            })
-            .filter((servico) => {
-                return servico.title
-                    .toLowerCase()
-                    .includes(this.props.nome.toLowerCase());
-            })
-            .sort((a, b) => {
-                switch (this.props.valorDeSorteio) {
-                    case "titulo":
-                        return this.props.ordem * a.title.localeCompare(b.title)
-                    case "data":
-                        return this.props.ordem * (new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime())
-                    default:
-                        return this.props.ordem * (a.price - b.price)
-                }
-            })
-
-            .map((servico) => {
-                return (
-                    <CardBox>
-                        <h2>{servico.title}</h2>
-                        <p>Disponivel até {this.conversorData(servico.dueDate)}</p>
-                        <p>Por apenas R$ {servico.price.toFixed(2)}</p> {/*toFixed(2) faz aparecer até duas casas decimais após o valor, sendo util até para valores em centavos*/}
-                        <div>
-                            <button>Mais Detalhes</button>
-                            <img src={carrinho} />
-                        </div>
-                    </CardBox>
-                )
-            })
-
-        // console.log(this.state.servicos)
+      .map((servico) => {
         return (
-            <MainContainer>
-                <div>
-                    <Filtro
-                        servicos={this.props.servicos}
-                        min={this.props.min}
-                        max={this.props.max}
-                        nome={this.props.nome}
-                        onChangeMin={this.props.onChangeMin}
-                        onChangeMax={this.props.onChangeMax}
-                        onChangeNome={this.props.onChangeNome}
-                        filtros={this.props.filtros}
-                    />
-                    <Ordenação
-                        ordem={this.props.ordem}
-                        valorDeSorteio={this.props.valorDeSorteio}
-                        onChangeOrdem={this.props.onChangeOrdem}
-                        onChangeSorteio={this.props.onChangeSorteio}
-                    />
-                </div>
-                <ListaServico>
-                    {listaDeServicos}
-                </ListaServico>
-
-            </MainContainer>
-        )
-    }
+          <CardBox>
+            <h2>{servico.title}</h2>
+            <p>Disponivel até {this.conversorData(servico.dueDate)}</p>
+            <p>Por apenas R$ {servico.price.toFixed(2)}</p>{" "}
+            {/*toFixed(2) faz aparecer até duas casas decimais após o valor, sendo util até para valores em centavos*/}
+            <div>
+              <button>Mais Detalhes</button>
+              <img
+                onClick={() => this.props.adicionaCarrinho(servico.id)}
+                src={carrinho}
+              />
+            </div>
+          </CardBox>
+        );
+      });
+    return (
+      <MainContainer>
+        <div>
+          <Filtro
+            servicos={this.props.servicos}
+            min={this.props.min}
+            max={this.props.max}
+            nome={this.props.nome}
+            onChangeMin={this.props.onChangeMin}
+            onChangeMax={this.props.onChangeMax}
+            onChangeNome={this.props.onChangeNome}
+            filtros={this.props.filtros}
+          />
+          <Ordenação
+            ordem={this.props.ordem}
+            valorDeSorteio={this.props.valorDeSorteio}
+            onChangeOrdem={this.props.onChangeOrdem}
+            onChangeSorteio={this.props.onChangeSorteio}
+          />
+        </div>
+        <ListaServico>{listaDeServicos}</ListaServico>
+      </MainContainer>
+    );
+  }
 }
 
-export default ListaServicos
+export default ListaServicos;
